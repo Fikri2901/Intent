e8b27981ee03254b0cc5ac4c5e8071a2502a98fb
Fikri2901 <fikriismudi@gmail.com>
2020-02-17 20:58:51 +0700

    menambahkan explicit intent

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -3,20 +3,30 @@ package id.ac.polinema.intent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText usernameInput;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameInput = findViewById(R.id.input_username);
+
     }
 
     public void handleExplicitIntent(View view) {
         Intent intent =  new Intent(this, ExplicitIntentActivity.class);
         startActivity(intent);
+    }
+
+    public void handleExit(View view) {
         finish();
     }
 }
